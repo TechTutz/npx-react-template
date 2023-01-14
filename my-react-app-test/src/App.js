357fe4e0654a723daf4a149c6a49bf972f385eb8
TechTutz <95581907+TechTutz@users.noreply.github.com>
2023-01-14 13:27:53 -0600

    get app into repo

---
+++ my-react-app-test/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+import LogoComponent from './LogoComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LogoComponent />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app-test/src/LogoComponent.js
@@ -0,0 +1,7 @@
+import logo from './logo.svg';
+
+function LogoComponent() {
+  return <img src={logo} className="App-logo" alt="logo" />;
+}
+
+export default LogoComponent;
